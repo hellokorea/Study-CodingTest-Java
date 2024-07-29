0c24f3fa578b0883878f1bc89007154cfd8e681b
hellokorea <wjdehddk123@naver.com>
2024-07-29 22:33:39 +0900

    01

--- Array/_01_array.java
+++ Array/_01_array.java
@@ -1,8 +1,34 @@
 package Array;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class _01_array {
 
     public static void main(String[] args) {
-        System.out.println("hello");
+        List<Integer> list = new ArrayList<>();
+        list.add(5);
+        list.add(11);
+
+        System.out.println(list);
+
+        // (list Type) 선언과 동시에 초기화 가능
+        List<Integer> list2 = new ArrayList<>(list);
+
+        System.out.println(list2);
+
+        int getEl = list.get(1);
+
+        System.out.println(getEl);
+
+        int size = list.size();
+
+        System.out.println(size);
+
+        // 삭제 된 원소 값 return
+        int r = list.remove(0);
+
+        System.out.println(r);
+        System.out.println(list);
     }
 }
--- Array/partice/_01_배열정렬하기.java
+++ Array/partice/_01_배열정렬하기.java
@@ -1,5 +1,30 @@
 package Array.partice;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class _01_배열정렬하기 {
 
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = 5;
+        int[] arr = new int[T];
+        Integer[] arr2 = new Integer[T];
+
+        for (int i = 0; i < T; i++) {
+            int el = Integer.parseInt(br.readLine());
+            arr[i] = el;
+            arr2[i] = el;
+        }
+
+        Arrays.sort((arr));
+        Arrays.sort(arr2, (a, b) -> Integer.compare(a, b));
+
+        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(arr2));
+
+    }
 }
